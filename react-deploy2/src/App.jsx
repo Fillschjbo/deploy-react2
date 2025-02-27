aa34748d74731c73360328b675850a7f7ace3dbb
fillip husebø <felipehusebo@gmail.com>
2025-02-27 10:29:03 +0100

    added pictures of dog

--- react-deploy2/src/App.jsx
+++ react-deploy2/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Link} from "react-router-dom";
+import {Dog} from "./component/Dog/index.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <h1>i have successfully deployed</h1>
         <Link to={"/test"}>to test page</Link>
         <a href="/test">to test </a>
+      <Dog/>
     </>
   )
 }
---
+++ react-deploy2/src/component/Dog/index.jsx
@@ -0,0 +1,10 @@
+export function Dog() {
+    return(
+        <>
+            <img src="https://image.forskning.no/2305920.jpg?imageId=2305920&x=0&y=26.39&cropw=100&croph=71.67&panox=0&panoy=26.39&panow=100&panoh=71.67&width=1200&height=683" alt="image of dog"/>
+            <img src="./2305920.jpg" alt=""/>
+            <img src="/public/2305920.jpg" alt=""/>
+
+        </>
+    )
+}
